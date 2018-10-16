bc43c49afb5de66270a66c16bf83092430ac65ce
yantao <yantao@hnwdkj.com>
2018-10-16 17:56:15 +0800

    email

--- report-analysis/src/main/java/com/wd/cloud/reportanalysis/controller/DefiningDataController.java
+++ report-analysis/src/main/java/com/wd/cloud/reportanalysis/controller/DefiningDataController.java
@@ -11,48 +11,24 @@ public class DefiningDataController {
 
     @RequestMapping("/getdefiningdata")
     public String tesg(@RequestParam String act, @RequestParam String table, @RequestParam int scid, @RequestParam String compare_scids, @RequestParam String time, @RequestParam String source, @RequestParam int signature, @RequestParam String category_type,Model model){
-
+        model.addAttribute("act", act);
+        model.addAttribute("table", table);
+        model.addAttribute("scid", scid);
+        model.addAttribute("compare_scids", compare_scids);
+        model.addAttribute("time", time);
+        model.addAttribute("source", source);
+        model.addAttribute("signature", signature);
+        model.addAttribute("category_type", category_type);
         if(act.equalsIgnoreCase("amount")&&table.equalsIgnoreCase("amount")) {
-            model.addAttribute("act", act);
-            model.addAttribute("table", table);
-            model.addAttribute("scid", scid);
-            model.addAttribute("compare_scids", compare_scids);
-            model.addAttribute("time", time);
-            model.addAttribute("source", source);
-            model.addAttribute("signature", signature);
-            model.addAttribute("category_type", category_type);
             return "forward:/hut";
         }
         if(act.equalsIgnoreCase("partition")&&table.equalsIgnoreCase("jcr")){
-            model.addAttribute("act", act);
-            model.addAttribute("table", table);
-            model.addAttribute("scid", scid);
-            model.addAttribute("compare_scids", compare_scids);
-            model.addAttribute("time", time);
-            model.addAttribute("source", source);
-            model.addAttribute("signature", signature);
-            model.addAttribute("category_type", category_type);
             return "forward:/fenqu";
         }
         if(act.equalsIgnoreCase("cited")&&table.equalsIgnoreCase("total_cited")){
-            model.addAttribute("act", act);
-            model.addAttribute("table", table);
-            model.addAttribute("scid", scid);
-            model.addAttribute("compare_scids", compare_scids);
-            model.addAttribute("time", time);
-            model.addAttribute("source", source);
-            model.addAttribute("signature", signature);
-            model.addAttribute("category_type", category_type);
             return "forward:/frequency";
         }
         if(act.equalsIgnoreCase("esi")&&table.equalsIgnoreCase("amount")){
-            model.addAttribute("act", act);
-            model.addAttribute("table", table);
-            model.addAttribute("scid", scid);
-            model.addAttribute("compare_scids", compare_scids);
-            model.addAttribute("time", time);
-            model.addAttribute("source", source);
-            model.addAttribute("signature", signature);
             model.addAttribute("category_type", category_type);
             return "forward:/esi";
         }
