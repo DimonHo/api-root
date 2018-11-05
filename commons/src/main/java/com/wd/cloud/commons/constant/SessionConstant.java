02378c820e09336342eb82b602923b682f69d37c
何志刚 <hezhigang@hnwdkj.com>
2018-11-05 08:42:43 +0800

    commons包修改

--- commons/src/main/java/com/wd/cloud/commons/constant/SessionConstant.java
+++ commons/src/main/java/com/wd/cloud/commons/constant/SessionConstant.java
@@ -7,7 +7,19 @@ package com.wd.cloud.commons.constant;
  */
 public class SessionConstant {
 
+    /**
+     * 登陆用户
+     */
     public static final String LOGIN_USER = "login_user";
 
+    /**
+     * 登陆用户账户所属机构
+     */
+    public static final String USER_ORG = "user_org";
+
+    /**
+     * 登陆ip所属机构
+     */
+    public static final String IP_ORG = "ip_org";
 
 }
