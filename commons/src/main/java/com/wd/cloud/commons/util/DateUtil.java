d8ae5608618ce39293f201227d1ca8ac32fcf89a
何志刚 <hezhigang@hnwdkj.com>
2018-11-30 14:10:22 +0800

    添加format方法

--- commons/src/main/java/com/wd/cloud/commons/util/DateUtil.java
+++ commons/src/main/java/com/wd/cloud/commons/util/DateUtil.java
@@ -1,5 +1,7 @@
 package com.wd.cloud.commons.util;
 
+import java.util.List;
+
 /**
  * @author He Zhigang
  * @date 2018/11/26
@@ -32,5 +34,36 @@ public class DateUtil extends cn.hutool.core.date.DateUtil {
                 break;
         }
         return dateFormatStr;
+    }
+
+    /**
+     * 格式化mysql时间
+     * @param type
+     * @return
+     */
+    public static String formatStr(int type){
+        String dateFormatStr = null;
+        switch (type) {
+            case 1:
+                dateFormatStr = "yyyy-MM-dd HH";
+                break;
+            case 2:
+                dateFormatStr = "yyyy-MM-dd";
+                break;
+            case 3:
+                dateFormatStr = "yyyy-MM";
+                break;
+            case 4:
+                dateFormatStr = "yyyy";
+                break;
+            default:
+                dateFormatStr = "yyyy-MM-dd HH:mm";
+                break;
+        }
+        return dateFormatStr;
+    }
+
+    public List<String> range(String start,String end){
+        return null;
     }
 }
