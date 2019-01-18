451a9eb0e27823b75dc4eba5968a5181bd87d989
wuqilong <wuqilong@hnwdkj.com>
2019-01-18 15:29:09 +0800

    解决命名冲突

--- doc-delivery/src/main/java/com/wd/cloud/docdelivery/controller/FrontendController.java
+++ doc-delivery/src/main/java/com/wd/cloud/docdelivery/controller/FrontendController.java
@@ -70,7 +70,7 @@ public class FrontendController {
     @ApiOperation(value = "文献求助")
     @PostMapping(value = "/help/form")
     public ResponseModel<HelpRecord> helpFrom(@Valid HelpRequestModel helpRequestModel, HttpServletRequest request) {
-        UserDTO userDTO1 =(UserDTO) request.getSession().getAttribute(SessionConstant.LOGIN_USER);
+        UserDTO userDTO = (UserDTO) request.getSession().getAttribute(SessionConstant.LOGIN_USER);
         //获取邮箱
         String helperEmail = helpRequestModel.getHelperEmail();
         //获取求助总条数
@@ -80,7 +80,7 @@ public class FrontendController {
         //获取权限
         Integer level = (Integer) request.getSession().getAttribute(SessionConstant.LEVEL);
         String finish = "文献求助记录已用完。";
-        if (userDTO1 == null){
+        if (userDTO == null){
             //获取学校信息
             OrgDTO attribute1 =(OrgDTO) request.getSession().getAttribute(SessionConstant.IP_ORG);
             long orgId = attribute1.getId();
@@ -94,7 +94,7 @@ public class FrontendController {
             }
         }else {
             //获取学校信息
-            OrgDTO org = userDTO1.getOrg();
+            OrgDTO org = userDTO.getOrg();
             long orgId = 0;
             if (org!= null){
                 orgId = org.getId();
@@ -121,7 +121,7 @@ public class FrontendController {
         Literature literature = new Literature();
         BeanUtil.copyProperties(helpRequestModel, helpRecord);
         BeanUtil.copyProperties(helpRequestModel, literature);
-        UserDTO userDTO = (UserDTO) request.getSession().getAttribute(SessionConstant.LOGIN_USER);
+
         OrgDTO orgDTO = (OrgDTO) request.getSession().getAttribute(SessionConstant.IP_ORG);
         if (userDTO != null) {
             helpRecord.setHelperId(userDTO.getId());
