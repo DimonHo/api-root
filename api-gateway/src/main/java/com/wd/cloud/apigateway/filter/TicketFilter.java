6bdd2383023315cef6bd71a2cdc3e911a43687fa
何志刚 <hezhigang@hnwdkj.com>
2019-02-23 16:37:33 +0800

    st获取错误问题

--- api-gateway/src/main/java/com/wd/cloud/apigateway/filter/TicketFilter.java
+++ api-gateway/src/main/java/com/wd/cloud/apigateway/filter/TicketFilter.java
@@ -1,17 +1,13 @@
 package com.wd.cloud.apigateway.filter;
 
-import cn.hutool.core.lang.Console;
-import cn.hutool.json.JSONUtil;
+import cn.hutool.core.util.StrUtil;
 import com.wd.cloud.apigateway.config.ParameterRequestWrapper;
 import com.wd.cloud.apigateway.utils.CasUtil;
 import com.wd.cloud.apigateway.utils.HttpUtil;
-import com.wd.cloud.commons.exception.AuthException;
-import com.wd.cloud.commons.model.ResponseModel;
 import lombok.extern.slf4j.Slf4j;
 import org.jasig.cas.client.Protocol;
 import org.jasig.cas.client.configuration.ConfigurationKeys;
 import org.jasig.cas.client.util.AbstractCasFilter;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -20,7 +16,6 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * @author He Zhigang
@@ -57,10 +52,10 @@ public class TicketFilter extends AbstractCasFilter {
             String clientUrl = this.constructServiceUrl(request, response);
             // 获取一个ST
             String st = CasUtil.getSt(requestWrapper, casServerLoginUrl, clientUrl, cookieStr);
-            log.info("获取ST=====================[{}]",st);
-            if (st != null) {
+            log.info("获取ST=====================[{}]", st);
+            if (StrUtil.isNotBlank(st)) {
                 //将ST加入到请求地址后面
-                requestWrapper.addQueryString("ticket="+st);
+                requestWrapper.addQueryString("ticket=" + st);
                 filterChain.doFilter(requestWrapper, response);
             }
         }
