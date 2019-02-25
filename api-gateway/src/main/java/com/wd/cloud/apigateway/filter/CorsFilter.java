113bbd9192eccda7288514e9d7871ec31941fcbb
何志刚 <hezhigang@hnwdkj.com>
2019-02-25 15:40:38 +0800

    Method PATCH is not allowed by Access-Control-Allow-Methods in preflight response

--- api-gateway/src/main/java/com/wd/cloud/apigateway/filter/CorsFilter.java
+++ api-gateway/src/main/java/com/wd/cloud/apigateway/filter/CorsFilter.java
@@ -25,7 +25,7 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Credentials","true");
-        response.setHeader("Access-Control-Request-Methods","GET,POST,OPTIONS,PUT,DELETE,PATCH");
+        response.setHeader("Access-Control-Allow-Methods","GET,POST,OPTIONS,PUT,DELETE,PATCH");
         response.setHeader("Access-Control-Allow-Headers", "*");
         filterChain.doFilter(request, response);
     }
