03c44e53bbb17195a333a78d512ca1c42024b54f
lujingfeng <lujingfeng@hnwdkj.com>
2019-03-28 17:39:46 +0800

    update libgenAllSearchController

--- pdfsearch-server/src/main/java/com/wd/cloud/pdfsearchserver/repository/imp/ElasticRepositoryImp.java
+++ pdfsearch-server/src/main/java/com/wd/cloud/pdfsearchserver/repository/imp/ElasticRepositoryImp.java
@@ -22,4 +22,8 @@ public class ElasticRepositoryImp implements ElasticRepository {
     public SearchResponse queryByName(String indexName, String type, QueryBuilder qeryBuilder) {
         return transportClient.prepareSearch(indexName).setTypes(type).setQuery(qeryBuilder).setSize(10).execute().actionGet();
     }
+
+    public SearchResponse queryAllTypeByQueryBuilder(String indexName, QueryBuilder qeryBuilder){
+        return transportClient.prepareSearch(indexName).setQuery(qeryBuilder).setSize(10).execute().actionGet();
+    }
 }
