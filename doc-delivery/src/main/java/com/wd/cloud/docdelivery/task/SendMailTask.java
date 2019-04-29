b16c05e874f7da12e53ac4df40b37bac9ed16bc8
Dimon Ho <hezhigang@hnwdkj.com>
2019-04-29 20:21:43 +0800

    定时任务修改

--- doc-delivery/src/main/java/com/wd/cloud/docdelivery/task/SendMailTask.java
+++ doc-delivery/src/main/java/com/wd/cloud/docdelivery/task/SendMailTask.java
@@ -4,6 +4,10 @@ import com.wd.cloud.docdelivery.pojo.entity.VHelpRecord;
 import com.wd.cloud.docdelivery.repository.VHelpRecordRepository;
 import com.wd.cloud.docdelivery.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -25,10 +29,11 @@ public class SendMailTask {
     @Autowired
     MailService mailService;
 
-    @Scheduled(cron = "0 0/1 * * * ?")
+    @Scheduled(cron = "0/15 * * * * ?")
     public void updateGiveRecord() {
-        //查询所有状态为未发送的求助记录
-        List<VHelpRecord> bySend = vHelpRecordRepository.findBySend(false);
+        //每隔15秒重发一封邮件，避免邮件发送频率过快
+        Pageable pageable = PageRequest.of(0,1, Sort.by(Sort.Order.by("gmtCreate")));
+        Page<VHelpRecord> bySend = vHelpRecordRepository.findBySend(false,pageable);
         bySend.forEach(vHelpRecord -> mailService.sendMail(vHelpRecord));
     }
 }
